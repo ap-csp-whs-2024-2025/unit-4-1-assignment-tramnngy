ec33189356545ae2026bc61e6d608cde66f4f2aa
Nhat Tram Nguyen <145398919+tramnngy@users.noreply.github.com>
2024-12-11 21:48:36 +0000

    printed out variables

--- classwork.cpp
+++ classwork.cpp
@@ -3,14 +3,15 @@
 
 int main()
 {
- int age;
- age = 15;
-
+ int age = 10;
+ // SET age to 10
  double price = 19.99;
-
-  int pet= 1;
-  int grade = 10;
-  // write code here
-  
+ bool hasPets = true;
+ std::cout <<age <<"\n" <<price <<std::endl <<hasPets <<std::endl;
+// \n is a new line
+// std::endl is a new line
+// std::cout << computer output
+std::cout <<"I am" <<" " <<age <<" " <<"years old.\n";
+std::cout <<"The shirt worths" <<" " <<price <<" " <<"dollars.\n";
   return 0;
 }
