d400b7753a85f096a240cdfcd925e4419e70f70c
Nhat Tram Nguyen <145398919+tramnngy@users.noreply.github.com>
2024-12-13 22:28:40 +0000

    finished up to excercise #3

--- classwork.cpp
+++ classwork.cpp
@@ -3,15 +3,42 @@
 
 int main()
 {
- int age = 10;
- // SET age to 10
- double price = 19.99;
- bool hasPets = true;
- std::cout <<age <<"\n" <<price <<std::endl <<hasPets <<std::endl;
-// \n is a new line
-// std::endl is a new line
-// std::cout << computer output
-std::cout <<"I am" <<" " <<age <<" " <<"years old.\n";
-std::cout <<"The shirt worths" <<" " <<price <<" " <<"dollars.\n";
-  return 0;
+//Exercise 1:
+int x = 25;
+int y = 3;
+std::cout <<"EXERCISE 1: " <<std::endl;
+std::cout <<"x + y = " << x + y <<std::endl;
+std::cout <<"x - y = " << y - x <<std::endl;
+std::cout <<"x * y = " << x * y <<std::endl;
+std::cout <<"x / y = " << x / y <<std::endl;
+std::cout <<"x % y = " << x % y <<std::endl;
+
+std::cout <<" " <<std::endl;
+
+//Exercise 2: 
+bool isSunny = true;
+bool hasUmbrella = false; 
+std::cout <<"EXERCISE 2: " <<std::endl;
+
+std::cout <<"isSunny && hasUmbrella is " <<(isSunny && hasUmbrella) <<std::endl;
+std::cout <<"isSunny || hasUmbrella is " <<(isSunny || hasUmbrella) <<std::endl;
+std::cout <<"!isSunny is " <<(!isSunny) <<std::endl;
+std::cout <<"!isSunny && !hasUmbrella is " <<(!isSunny && !hasUmbrella) <<std::endl;
+
+std::cout <<" " <<std::endl;
+
+//Exercise 3: 
+int n = 7;
+double m = 4.0;
+bool hasLicense = false;
+std::cout <<"EXERCISE 3: " <<std::endl;
+
+std::cout <<"n > m  is " << (n > m)  <<std::endl;
+std::cout <<"m = 4.0 is " << (m == 4.0)  <<std::endl;
+std::cout <<"m <= n && hasLicense is " << (m <= n && hasLicense) <<std::endl;
+std::cout <<"n > 5 && hasLicense == false is " << (n > 5 && hasLicense == false) <<std::endl;
+
+
+
+ return 0;
 }
